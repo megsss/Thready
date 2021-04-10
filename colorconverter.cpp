221f74c28378e470e4e4ee7b12d1b6394b67013d
Megan Stucky <mstucky1@msudenver.edu>
2021-04-10 12:39:52 -0600

    more methods in color converter class

--- colorconverter.cpp
+++ colorconverter.cpp
@@ -1,5 +1,6 @@
 #include "colorconverter.h"
 #include <QColor>
+#include <QDebug>
 
 ColorConverter::ColorConverter()
 {
@@ -9,16 +10,38 @@ ColorConverter::ColorConverter()
 QColor ColorConverter::RGBtoDMC(QColor color)
 {
     QColor pickedColor = color;
+    int redValue = color.red();
+    int blueValue = color.blue();
+    int greenValue = color.green();
+    //qDebug << "red color value is" + redValue ;
 
     //TODO
 }
 
 QColor ColorConverter::DMCtoRGB(QColor color)
 {
-    //TODO
+    return color.toRgb();
 }
 
 void ColorConverter::findClosestColor(QColor color)
 {
 
+    //compareColors();
 }
+
+void ColorConverter::compareColors(QColor dmcColor, QColor color)
+{
+    int distance;
+    int redToCompare = color.red();
+    int greenToCompare = color.green();
+    int blueToCompare = color.blue();
+
+    int dmcColorRed = dmcColor.red();
+    int dmcColorGreen = dmcColor.green();
+    int dmcColorBlue = dmcColor.blue();
+    //distance = sqrt((redToCompare - dmcColorRed)^2 + (greenToCompare - dmcColorGreen)^2 + (blueToCompare - dmcColorBlue)^2);
+
+
+
+}
+
