140a77644bef1a0e0493bd1ee76ddb1f166cc1c9
Megan Stucky <mstucky1@msudenver.edu>
2021-04-26 18:51:01 -0600

    fix dmcFloss object parameters

--- dmcfloss.h
+++ dmcfloss.h
@@ -8,14 +8,14 @@ class DMCFloss: QObject
 {
     Q_OBJECT
 public:
-    DMCFloss();
+    DMCFloss(QString flossNum, QString name, QColor rgbVal);
     QColor color() const { return color_; }
     QRgb qrgb() const { return color_.rgb(); }
-    int code() const { return code_; }
+    QString flossNum() const { return flossNum_; }
     QString name() const { return name_; }
 
 private:
-    int code_;
+    QString flossNum_;
     QString name_;
     QColor color_;
 };
