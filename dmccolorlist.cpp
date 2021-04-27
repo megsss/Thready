e6e24ab2cc3a316e211650487475a1ef5f8dce9c
Megan Stucky <mstucky1@msudenver.edu>
2021-04-26 18:32:25 -0600

    delete dmcColors.txt, replace with csv file

--- dmccolorlist.cpp
+++ dmccolorlist.cpp
@@ -11,11 +11,19 @@ dmcColorList::dmcColorList()
 //read DMC colors from dmcColors.txt and add to array
 void dmcColorList::initializeDMCList()
 {
-    QFile dmcFile(":/dmcColors.txt");
+    QFile dmcFile(":/dmcColorValues.csv");
     if(!dmcFile.open(QIODevice::ReadOnly | QIODevice::Text)){
         qDebug() << "File open failed";
         return;
     }
+
     QTextStream in(&dmcFile);
+
+    // read each line until '\n' (end of line)
+    // order: DMC Color #, Color Name, RGB Color
+    // save to dmcColor object
+    // add color to list of dmcColor objects
+
+    qDebug() << in.readLine();
     qDebug() << in.readAll();
 }
